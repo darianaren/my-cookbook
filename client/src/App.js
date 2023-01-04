4c79017fe164e4445c11c063eb12f7d4d8d35d50
Dariana Rengifo <darianarengifo@gmail.com>
2023-01-04 19:20:01 -0300

    update baseURL axios

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Message from "./layout/Message";
 import RouterFunction from "./routes";
 import axios from "axios";
 
-axios.defaults.baseURL = "my-cookbook-production.up.railway.app";
+axios.defaults.baseURL = "https://my-cookbook-production.up.railway.app/";
 
 function App() {
   return (
