b69a2f8580140f3dff0a9b17109614da38075bf5
Dariana Rengifo <darianarengifo@gmail.com>
2023-01-10 08:15:42 -0300

    correction routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 const routerDiet = require("./routeDiets/routeDiet");
+
 const routerRecipe = require("./routeRecipes/routeRecipe");
 const routerRecipeApi = require("./routeRecipes/routeRecipeApi");
 const routerRecipeDb = require("./routeRecipes/routeRecipeDb");
